820ffe6fb554fdd402dfb2a5475a821c8cec707c
veevtli <maury21morales@gmail.com>
2026-06-09 17:34:05 -0700

    1.1.3

--- scriptXRS.js
+++ scriptXRS.js
@@ -30,3 +30,39 @@ const buttons =
     		});
 
 		});
+
+  // 1. List all the image paths you want to cycle through
+  const images = [
+    "images/front.jpg",
+    "images/side.jpg",
+    "images/rear.jpg",
+    "images/int.jpg",
+	"images/dash.jpg"
+  ];
+
+  // 2. Start at the first image (index 0)
+  let currentIndex = 0;
+
+  function nextImage() {
+    // Move to the next index
+    currentIndex++;
+
+    // If we reach the end of the array, loop back to the beginning
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+
+    // Update the image src on the page
+    document.getElementById("matrix-image").src = images[currentIndex];
+  }
+
+  function prevImage() {
+  currentIndex--;
+  
+  // If we go past the beginning, loop to the last image
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  
+  document.getElementById("matrix-image").src = images[currentIndex];
+}
